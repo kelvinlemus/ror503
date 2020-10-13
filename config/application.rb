104360bee144b34bef2a6ac2f116ff45d15f89db
Kelvin Lemus <kelvinlemus@users.noreply.github.com>
2020-10-13 11:53:53 -0600

    :D

--- config/application.rb
+++ config/application.rb
@@ -37,5 +37,7 @@ module RorChallenge503
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.autoload = :classic
   end
 end
