cd33ebec4a18c06fd9bd5e62b881307a0fb9d6e7
Kelvin Lemus <kelvinlemus@users.noreply.github.com>
2020-10-13 11:58:55 -0600

    :D

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,4 +1,3 @@
-# INFO: This app does not require a database
-# class ApplicationRecord < ActiveRecord::Base
-#   self.abstract_class = true
-# end
+class ApplicationRecord < ActiveRecord::Base
+  self.abstract_class = true
+end
